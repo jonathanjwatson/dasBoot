44b7394855614000683d543da0c9b80ac3de8d0c
jwats287 <jwats287@users.noreply.github.com>
2019-04-21 18:28:19 -0400

    Dependencies added.

--- src/main/java/com/boot/App.java
+++ src/main/java/com/boot/App.java
@@ -1,13 +1,15 @@
 package com.boot;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
     }
 }
